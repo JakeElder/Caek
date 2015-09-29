e09c35ec4a87024fed30bd753ae5bdb2ed961605
Jake Elder <jake@jpeg.me.uk>
2015-09-29 11:37:35 +0100

    Lunch reccomendation engine

--- caek/service.js
+++ caek/service.js
@@ -15,7 +15,28 @@ class Service {
       cronTime: this.cronTime,
       onTick: function() {
         const food = this.getFood().toUpperCase();
-        this.hipchat.notify(`GO EAT SOME FUCKING ${food}`);
+        this.hipchat.notify('INITIALIZING FOOD RECOMMENDATION ENGINE...');
+        setTimeout(() => {
+          this.hipchat.notify('ANALYZING USER PREFERENCES BASED ON CHAT HISTORY...');
+        }, 10 * 1000);
+        setTimeout(() => {
+          this.hipchat.notify('GATHERING INFORMATION FROM SOCIAL MEDIA...');
+        }, 20 * 1000);
+        setTimeout(() => {
+          this.hipchat.notify('taking a quick break');
+        }, 30 * 1000);
+        setTimeout(() => {
+          this.hipchat.notify('COMPILING A LIST OF POSSIBLE VENUES BASED ON LOCATION...');
+        }, 40 * 1000);
+        setTimeout(() => {
+          this.hipchat.notify('WEIGHTING CHOICES BASED ON PREFERENCE, PROXIMITY AND WEATHER CONDITIONS...');
+        }, 50 * 1000);
+        setTimeout(() => {
+          this.hipchat.notify('DISREGARDING COLLECTED DATA AND SELECTING FOOD RANDOMLY BASED ON A LIST OF THREE DIFFERENT THINGS...');
+        }, 60 * 1000);
+        setTimeout(() => {
+          this.hipchat.notify(`${food}?`);
+        }, 70 * 1000);
       }.bind(this),
       start: true
     });
